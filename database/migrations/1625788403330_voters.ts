16476bb51c8fb5838e204882f2d1b325d379d29c
joao <joaovlima07@hotmail.com>
2021-07-08 21:21:24 -0300

    did stuff

--- database/migrations/1625788403330_voters.ts
+++ database/migrations/1625788403330_voters.ts
@@ -1,23 +1,23 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
-export default class Candidates extends BaseSchema {
-  protected tableName = 'candidates'
+export default class Voters extends BaseSchema {
+  protected tableName = 'voters'
 
-  public async up() {
+  public async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
+      table.increments('id')
 
       table.string('cpf').notNullable()
       table.string('name').notNullable()
       table.string('email').notNullable()
-      table.integer('voting_number').notNullable()
-
+      table.string('password').nullable()
+    
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
   }
 
-  public async down() {
+  public async down () {
     this.schema.dropTable(this.tableName)
   }
 }
